41d3352ae7cc36718ddcb870cb8cc55e5ed6f428
highstar97 <pgt0185@gmail.com>
2021-10-07 17:14:44 +0900

    Resolve 위장

--- Hash/Disquise.cpp
+++ Hash/Disquise.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <unordered_map>
 
-int solution(std::vector<std::vector<std::string>> clothes) {
+int solution(std::vector<std::vector<std::string>> clothes)
+{
     int answer = 1;
-    std::unordered_map<std::string, int> u_map;
-    for(auto data : clothes){
-        if(u_map.find(data[1]) == u_map.end())
-            u_map[data[1]] = 1;
+    std::unordered_map<std::string,int> UMap;
+    for(auto data : clothes)
+    {
+        if(UMap.find(data[1]) == UMap.end())
+            UMap[data[1]] = 1;
         else
-            u_map[data[1]]++;
-    }
-    for(auto data : u_map){
-        answer *= (data.second + 1);
+            UMap[data[1]]++;
     }
 
-    return answer - 1;
+    for(auto data : UMap)
+        answer *= data.second + 1;
+    
+    return answer-1;
 }
 
 int main(){
