a615f52089c6940be8068120ee93d2e503c8e518
highstar97 <pgt0185@gmail.com>
2021-10-07 23:35:05 +0900

    Resolve 베스트앨범

--- Hash/Best_Album.cpp
+++ Hash/Best_Album.cpp
@@ -1,62 +1,56 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 
-using namespace std;
+#define si std::pair<std::string, int>
 
-vector<int> solution(vector<string> genres, vector<int> plays) {
-    vector<int> answer;
-    unordered_map<string,vector<int>> map_genres_index;
-    unordered_map<string,int> map_genres_plays;
-
-    for(int i=0; i<genres.size(); i++){
-        map_genres_index[genres[i]].emplace_back(i);
-        map_genres_plays[genres[i]] += plays[i];
-    }
-
-    // 많이 재생된 순으로 genres_list에 넣음
-    vector<string> genres_list;
-    for(auto it=map_genres_plays.begin(); it != map_genres_plays.end(); it++){
-        auto max_index = it;
-        for(auto it2=map_genres_plays.begin(); it2 !=map_genres_plays.end(); it2++){
-            if(max_index->second < it2->second){
-                max_index = it2;
-            }
-        }
-        max_index->second = 0;
-        genres_list.emplace_back(max_index->first);
-    }
-
-    // 많이 재생된 장르 순으로 출력
-    for(int i=0; i<genres_list.size(); i++){
-        pair<int,int> index_play1 = make_pair(0,0);
-        pair<int,int> index_play2 = make_pair(0,0);
-        // 가장 많이 재생된 앨범 번호와 재생횟수를 pair로 저장
-        for(auto data : map_genres_index[genres_list[i]]){
-            if(plays[data] > index_play1.second){
-                index_play2.first = index_play1.first;
-                index_play2.second = index_play1.second;
-                index_play1.first = data;
-                index_play1.second = plays[data];
-            }
-            else if(plays[data] > index_play2.second){
-                index_play2.first = data;
-                index_play2.second = plays[data];
-            }
-        }
-
-        answer.emplace_back(index_play1.first);
-        if(index_play2.second != 0)
-            answer.emplace_back(index_play2.first);
+std::vector<int> solution(std::vector<std::string> genres, std::vector<int> plays)
+{
+    std::vector<int> answer;
+    std::unordered_map<std::string, int> Genre_TotalPlays;
+    std::unordered_map<std::string, std::vector<int>> Genre_Plays;
+    for(int i=0; i<genres.size(); i++)
+    {
+        if(Genre_TotalPlays.find(genres[i]) == Genre_TotalPlays.end())
+            Genre_TotalPlays[genres[i]] = plays[i];
+        else
+            Genre_TotalPlays[genres[i]] += plays[i];
+        Genre_Plays[genres[i]].emplace_back(plays[i]);
     }
     
+    std::vector<si> v1(Genre_TotalPlays.begin(), Genre_TotalPlays.end());
+    sort(v1.begin(),v1.end(),[](si& a, si& b){
+        return a.second > b.second;
+    });
+
+    for(auto& data : Genre_Plays)
+    {
+        sort(data.second.begin(), data.second.end(), [](int &a, int &b)->bool{
+            return a > b;
+        });
+    }
+
+    for(auto data : v1){
+        int count = 0;
+        int max = (Genre_Plays[data.first].size() >= 2) ? 2 : 1;
+        do{
+            for(int i=0; i<genres.size(); i++){
+                if(genres[i] == data.first && plays[i] == Genre_Plays[data.first][count]){
+                    answer.emplace_back(i);
+                    plays[i] = 0;
+                }
+            }
+            count++;
+        }while(count < max);
+    }
+
     return answer;
 }
 
 int main(){
     std::vector<std::string> genres = {"classic", "pop", "classic", "classic","jazz","pop", "Rock", "jazz"};
-    std::vector<int> plays = {150, 600, 150, 800, 1100, 2500, 100, 1000};
+    std::vector<int> plays = {150, 600, 150, 800, 1100, 2500, 1000, 1000};
     for(auto data : solution(genres,plays))
         std::cout << data << " ";
 }
