73514e44352d524a3478c722bbc4743815c1eb41
highstar97 <pgt0185@gmail.com>
2021-10-12 18:17:38 +0900

    Resolve 더 맵게

--- Heap/more_spicy.cpp
+++ Heap/more_spicy.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 #include <vector>
 #include <queue>
 
@@ -7,28 +6,25 @@ using namespace std;
 
 int solution(vector<int> scoville, int K){
     int answer = 0;
-    // Min_Heap 생성과 동시에 scoville 정보를 Heap 안으로
     priority_queue<int, vector<int>, greater<int>> pq (scoville.begin(), scoville.end());
-
-    // 가장 작은 scoville 지수가 K 미만이라면 섞기 진행
-    while (pq.top() < K){
-        answer++;           // 섞기 진행 횟수 1회 추가
-        // 음식이 하나 남아서 더 이상 섞기가 불가능 할 경우
+    while (pq.top() < K)
+    {
+        answer++;  
         if(pq.size() == 1)
             return -1;
-        int min1 = pq.top();
+        int new_scoville = pq.top();
         pq.pop();
-        int min2 = pq.top();
+        new_scoville += pq.top()*2;
         pq.pop();
-        pq.emplace(min1 + (2 * min2));
+        pq.emplace(new_scoville);
     }
-
     return answer;
 }
 
 int main()
 {
-    vector<int> scoville = {3,100,65,43,1000000};
     int K = 1000000000;
-    cout << solution(scoville, K);
+    std::vector<int> scoville = {3,100,65,43,1000000};
+    std::cout << solution(scoville, K) << std::endl;
+    std::cout << solution({1,2,3},11);
 }
