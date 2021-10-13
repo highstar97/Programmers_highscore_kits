1d2d4668cc98a0f5cb7a77504dec3e7c5368a7b7
highstar97 <pgt0185@gmail.com>
2021-10-13 16:16:44 +0900

    Resolve K번째수

--- Sort/Kth_Number.cpp
+++ Sort/Kth_Number.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
-using namespace std;
-
-vector<int> solution(vector<int> array, vector<vector<int>> commands) {
-    vector<int> answer;
-    for(int i=0; i<commands.size(); i++){
-        vector<int> temp(array.begin()+(commands[i][0]-1), array.begin()+(commands[i][1]));
+std::vector<int> solution(std::vector<int> array, std::vector<std::vector<int>> commands)
+{
+    std::vector<int> answer;
+    for(auto data : commands)
+    {
+        std::vector<int> temp(array.begin()+data[0]-1,array.begin()+data[1]);
         sort(temp.begin(),temp.end());
-        answer.emplace_back(temp[commands[i][2]-1]);
+        answer.emplace_back(temp[data[2]-1]);
     }
     return answer;
 }
 
-int main(){
-    vector<int> array = {1, 5, 2, 6, 3, 7, 4};
-    vector<vector<int>> commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+int main()
+{
+    std::vector<int> array = {1, 5, 2, 6, 3, 7, 4};
+    std::vector<std::vector<int>> commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
     for(auto data : solution(array,commands))
-        cout << data << " ";
+        std::cout << data << " ";
 }
