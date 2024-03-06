f33904ecb43e069baabbdd1cb1a39afced06db68
highstar97 <pgt0185@gmail.com>
2024-03-06 20:06:29 +0900

    Resolve H_index

--- Sort/H_index.cpp
+++ Sort/H_index.cpp
@@ -2,7 +2,9 @@
 #include <vector>
 #include <algorithm>
 
-int solution(std::vector<int> citations)
+using namespace std;
+
+int solution(vector<int> citations)
 {
     int size = citations.size();
     sort(citations.begin(),citations.end());
@@ -15,9 +17,42 @@ int solution(std::vector<int> citations)
     }    
 }
 
+int solution2(vector<int> citations)
+{
+    sort(citations.begin(), citations.end());
+    
+    int answer, start = 0, end = citations.size();
+
+    while(start <= end)
+    {
+        int mid = (start+end)/2;
+        
+        int count = 0;
+        for(int i=0; i<citations.size(); ++i)
+        {
+            if(citations[i] >= mid)
+            {
+                ++count;
+            }
+        }
+        
+        if(count >= mid)
+        {
+            answer = mid;
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+    }
+    
+    return answer;
+}
+
 int main()
 {
     std::vector<int> citations = {0,1,2,3,3,3};
     std::cout << solution(citations) << std::endl;
-    std::cout << solution({0,2,4,5,6});
+    std::cout << solution2({0,2,4,5,6});
 }
