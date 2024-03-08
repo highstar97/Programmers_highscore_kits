fa680c0955241abd5c74a4f6dea04deca0cc3630
highstar97 <pgt0185@gmail.com>
2024-03-08 16:48:29 +0900

    Resolve Network

--- DFS_BFS/Network.cpp
+++ DFS_BFS/Network.cpp
@@ -1,47 +1,86 @@
 #include <iostream>
+#include <string>
 #include <vector>
+#include <set>
 
 using namespace std;
 
-const int MAX = 200;
+int FindParent(int x, vector<int> &Parents)
+{
+    if (Parents[x] == x)
+    {
+        return x;
+    }
+    return Parents[x] = FindParent(Parents[x], Parents);
+}
 
-void DFS_Recursion(int start, vector<int> graph[], bool visited[]){
-    int current_node = start;
-    visited[start] = true;
-    
-    for(int i=0; i<graph[current_node].size(); i++){
-        int next_node = graph[current_node][i];
-        if(visited[next_node] == false){
-            DFS_Recursion(next_node,graph,visited);
+void Union(int x, int y, vector<int> &Parents)
+{
+    x = FindParent(x, Parents);
+    y = FindParent(y, Parents);
+
+    x < y ? Parents[y] = x : Parents[x] = y;
+}
+
+int solution(int n, vector<vector<int>> computers)
+{
+    set<int> Networks;
+    vector<int> Parents(n);
+    for (int i = 0; i < n; ++i)
+    {
+        Parents[i] = i;
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (computers[i][j] == 1)
+            {
+                Union(i, j, Parents);
+            }
+        }
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        Networks.emplace(FindParent(i, Parents));
+    }
+
+    return Networks.size();
+}
+
+void DFS(int x, vector<bool> &IsVisited, const vector<vector<int>> &computers)
+{
+    IsVisited[x] = true;
+    for (int i = 0; i < computers.size(); ++i)
+    {
+        if (IsVisited[i] == false && computers[x][i] == 1)
+        {
+            DFS(i, IsVisited, computers);
         }
     }
 }
 
-int solution(const int n, vector<vector<int>> computers) {
+int solution2(int n, vector<vector<int>> computers)
+{
     int answer = 0;
-    bool visited[MAX] = {false};
-    vector<int> graph[n];
+    vector<bool> IsVisited(n, false);
 
-    // 인접 노드 정보 넣기 , 순서대로 넣기 때문에 sort할 필요 없음
-    for(int i=0; i<computers.size(); i++){
-        for(int j=0; j<computers.size(); j++){
-            if( i != j && computers[i][j] == 1){
-                graph[i].emplace_back(j);
-            }
-        }
-    }
-    // DFS를 돌고도 방문하지 않은 컴퓨터가 있다면 그건 다른 네트워크
-    for(int i=0; i<computers.size(); i++){
-        if(visited[i] == false){
-            DFS_Recursion(i,graph,visited);
-            answer++;
+    for (int i = 0; i < n; ++i)
+    {
+        if (IsVisited[i] == false)
+        {
+            DFS(i, IsVisited, computers);
+            ++answer;
         }
     }
     return answer;
 }
 
-int main(){
+int main()
+{
     int n = 3;
     vector<vector<int>> computers = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
-    cout << solution(n,computers);
+    cout << solution(n, computers) << " " << solution2(n, computers);
 }
