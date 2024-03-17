bf3dd7a602f72233f8f04313b51cf638e026c1f2
highstar97 <pgt0185@gmail.com>
2024-03-17 18:09:10 +0900

    Resolve More_Spicy

--- Heap/more_spicy.cpp
+++ Heap/more_spicy.cpp
@@ -4,27 +4,28 @@
 
 using namespace std;
 
-int solution(vector<int> scoville, int K){
-    int answer = 0;
-    priority_queue<int, vector<int>, greater<int>> pq (scoville.begin(), scoville.end());
+int solution(vector<int> scoville, int K)
+{
+    int count = 0;
+    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
     while (pq.top() < K)
     {
-        answer++;  
-        if(pq.size() == 1)
+        if (pq.size() == 1)
+        {
             return -1;
-        int new_scoville = pq.top();
-        pq.pop();
-        new_scoville += pq.top()*2;
-        pq.pop();
-        pq.emplace(new_scoville);
+        }
+        int newscoville = pq.top(); pq.pop();
+        newscoville += pq.top() * 2; pq.pop();
+        pq.emplace(newscoville);
+        ++count;
     }
-    return answer;
+    return count;
 }
 
 int main()
 {
     int K = 1000000000;
-    std::vector<int> scoville = {3,100,65,43,1000000};
+    std::vector<int> scoville = {3, 100, 65, 43, 1000000};
     std::cout << solution(scoville, K) << std::endl;
-    std::cout << solution({1,2,3},11);
+    std::cout << solution({1, 2, 3}, 11);
 }
