35bbb8f675aea066efc31f00eeeb7c7f98e4bb52
highstar97 <pgt0185@gmail.com>
2024-03-17 20:26:04 +0900

    Resolve Disk_Controller

--- Heap/Disk_Controller.cpp
+++ Heap/Disk_Controller.cpp
@@ -1,56 +1,69 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#define ii std::pair<int,int>
-auto cmp = [](ii& a, ii& b){
-    if(a.first == b.first)
-        return a.second > b.second;
-    else return a.first > b.first;
-    };
+#include <algorithm>
 
-auto cmp2 = [](ii&a, ii& b){
-    if(a.second == b.second)
-        return a.first > b.first;
-    else return a.second > b.second;
+using namespace std;
+
+struct CompareByStartTime
+{
+    bool operator()(const vector<int> &f, const vector<int> &b)
+    {
+        if (f[0] == b[0])
+        {
+            return f[1] > b[1];
+        }
+        return f[0] > b[0];
+    }
 };
 
-int solution(std::vector<std::vector<int>> jobs)
+struct CompareByActingTime
 {
-    int time;
-    int answer = 0;
-    std::priority_queue<ii,std::vector<ii>,decltype(cmp)> pq(cmp);
-    for(auto data : jobs)
-        pq.emplace(std::make_pair(data[0],data[1]));
-    
-    time = pq.top().first;
-    std::priority_queue<ii,std::vector<ii>,decltype(cmp2)> stack_pq(cmp2);
-    while (!pq.empty())
+    bool operator()(const vector<int> &f, const vector<int> &b)
     {
-        while (!pq.empty() && pq.top().first <= time)
+        return f[1] > b[1];
+    }
+};
+int solution(vector<vector<int>> jobs)
+{
+    int Answer = 0;
+    int CurrentTime = 0;
+    priority_queue<vector<int>, vector<vector<int>>, CompareByStartTime> HeapByStartTime(jobs.begin(), jobs.end());
+    priority_queue<vector<int>, vector<vector<int>>, CompareByActingTime> HeapByActingTime;
+
+    while (!HeapByStartTime.empty())
+    {
+        // 현재 시간까지 할 수 있는 모든 작업을 HeapByActingTime에 추가한다.
+        while (!HeapByStartTime.empty() && HeapByStartTime.top()[0] <= CurrentTime)
         {
-            stack_pq.emplace(pq.top());
-            pq.pop();
+            HeapByActingTime.emplace(HeapByStartTime.top());
+            HeapByStartTime.pop();
         }
-        if(stack_pq.empty())
-            time = pq.top().first;
+        // 현재시간까지 수행할 수 있는 작업이 없다면 가장 빠른 작업을 대기열에 추가한다.
+        if (HeapByActingTime.empty())
+        {
+            CurrentTime = HeapByStartTime.top()[0];
+        }
+        // 현재 시간에 수행할 수 있는 작업들 중, 소요시간이 가장 작은 작업부터 실행
         else
         {
-            answer += time + stack_pq.top().second - stack_pq.top().first;
-            time = time + stack_pq.top().second;
-            stack_pq.pop();
+            CurrentTime += HeapByActingTime.top()[1];
+            Answer += CurrentTime - HeapByActingTime.top()[0];
+            HeapByActingTime.pop();
         }
     }
-    while(!stack_pq.empty())
+    // HeapByActingTime을 모두 비웠을 때, 모든 작업 수행 완료된 것
+    while (!HeapByActingTime.empty())
     {
-        answer += time + stack_pq.top().second - stack_pq.top().first;
-        time = time + stack_pq.top().second;
-        stack_pq.pop();
+        CurrentTime += HeapByActingTime.top()[1];
+        Answer += CurrentTime - HeapByActingTime.top()[0];
+        HeapByActingTime.pop();
     }
-    return answer/jobs.size();
+    return Answer / jobs.size();
 }
 
 int main()
 {
-    std::vector<std::vector<int>> jobs = {{0,3},{3,8},{3,4},{6,1}};
-    std::cout << solution(jobs);
+    vector<vector<int>> jobs = {{0, 3}, {3, 8}, {3, 4}, {6, 1}};
+    cout << solution(jobs);
 }
