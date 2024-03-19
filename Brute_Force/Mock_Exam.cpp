d68cd3f919744caa0a3dfe09d3aa803e21f870e5
highstar97 <pgt0185@gmail.com>
2024-03-19 20:14:04 +0900

    Resolve Mock_Exam

--- Brute_Force/Mock_Exam.cpp
+++ Brute_Force/Mock_Exam.cpp
@@ -2,35 +2,41 @@
 #include <vector>
 #include <algorithm>
 
-std::vector<int> solution(std::vector<int> answers)
+using namespace std;
+
+vector<int> solution(vector<int> Answers)
 {
-    std::vector<int> answer;
-    std::vector<int> student1 = {1,2,3,4,5};
-    std::vector<int> student2 = {2,1,2,3,2,4,2,5};
-    std::vector<int> student3 = {3,3,1,1,2,2,4,4,5,5};
-    std::vector<int> score(3,0);
+    vector<int> Answer;
+    vector<vector<int>> Choices = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+    vector<int> Score(3, 0);
 
-    for(int i=0; i<answers.size(); i++)
+    for (int i = 0; i < Answers.size(); ++i)
     {
-        if(answers[i] == student1[i%student1.size()]) score[0]++;
-        if(answers[i] == student2[i%student2.size()]) score[1]++;
-        if(answers[i] == student3[i%student3.size()]) score[2]++;
+        for (int j = 0; j < 3; ++j)
+        {
+            if (Answers[i] == Choices[j][i % Choices[j].size()])
+            {
+                ++Score[j];
+            }
+        }
     }
 
-    int max = *max_element(score.begin(),score.end());
-
-    for(int i=0; i<score.size(); i++)
+    int MaxScore = *max_element(Score.begin(), Score.end());
+    for (int i = 0; i < Score.size(); ++i)
     {
-        if(score[i] == max)
-            answer.emplace_back(i+1);
+        if (Score[i] == MaxScore)
+        {
+            Answer.emplace_back(i+1);
+        }
     }
-
-    return answer;
+    return Answer;
 }
 
 int main()
 {
-    std::vector<int> answers = {1, 2, 3, 4, 5};
-    for(auto data : solution(answers))
-        std::cout << data;
+    vector<int> Answers = {1, 2, 3, 4, 5};
+    for (auto data : solution(Answers))
+    {
+        cout << data;
+    }
 }
